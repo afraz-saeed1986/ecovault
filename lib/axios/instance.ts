ee02fa5d06ed1bd61caf26ce20a16ca7569dce15
s.afraz <s.afraz@rayanbourse.ir>
2025-11-30 15:36:54 +0330

    change admin.ts....

--- lib/axios/instance.ts
+++ lib/axios/instance.ts
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!
 
 // نوع دقیق کوکی‌های Next.js
 type NextCookie = { name: string; value: string }
